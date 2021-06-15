ae8fb183196ae0e9810874764d840fd02702c8b8
wangtianyi <wangtianyi@goclouds.cn>
2021-06-15 10:54:38 +0800

    初始化项目

---
+++ huaweiCloud/src/main/java/com/hw/huaweicloud/HuaweicloudApplication.java
@@ -0,0 +1,12 @@
+package com.hw.huaweicloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuaweicloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuaweicloudApplication.class, args);
+    }
+}
---
+++ huaweiCloud/src/test/java/com/hw/huaweicloud/HuaweicloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hw.huaweicloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuaweicloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
